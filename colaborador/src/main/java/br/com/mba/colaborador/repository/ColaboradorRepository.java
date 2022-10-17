070e037ac168c93859ef18fbd3a7b480296b2617
Makio78 <marceloakiok@gmail.com>
2022-10-17 18:51:20 -0300

    Ajuste

--- colaborador/src/main/java/br/com/mba/colaborador/repository/ColaboradorRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.mba.colaborador.repository;
-
-import br.com.mba.colaborador.model.Colaborador;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ColaboradorRepository extends JpaRepository<Colaborador, Long> {
-
-}
--- colaborador/src/main/resources/db/migration/V1__criar_tabela_colaborador.sql
+++
@@ -1,6 +0,0 @@
-
-CREATE TABLE colaborador (
- id bigint(20) NOT NULL AUTO_INCREMENT,
- nome varchar(100) DEFAULT NULL,
- PRIMARY KEY (id)
-);
